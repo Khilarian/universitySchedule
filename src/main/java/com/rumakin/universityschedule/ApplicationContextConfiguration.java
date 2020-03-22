2861b845c370e2bca749c11f593ea80841093bc2
Kirill Rumakin <donnorvator@gmail.com>
2020-03-22 11:14:51 +0300

    Update spring config

--- src/main/java/com/rumakin/universityschedule/ApplicationContextConfiguration.java
+++ src/main/java/com/rumakin/universityschedule/ApplicationContextConfiguration.java
@@ -1,9 +1,7 @@
 package com.rumakin.universityschedule;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.*;
 
@@ -12,29 +10,26 @@ import org.springframework.jdbc.datasource.*;
 @PropertySource("classpath:config.properties")
 public class ApplicationContextConfiguration {
 
-    @Autowired
-    Environment environment;
-    
-    private static final String URL = "db.host";
-    private static final String USER = "db.login";
-    private static final String PASSWORD = "db.password";
-    private static final String DRIVER = "db.driver";
+    @Value("${db.driver}")
+    private String driver;
+    @Value("${db.host}")
+    private String url;
+    @Value("${db.login}")
+    private String login;
     @Value("${db.password}")
     private String password;
-    
-    
-    
+
     @Bean
     @Scope("singleton")
     public DriverManagerDataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getProperty(DRIVER));
-        dataSource.setUrl(environment.getProperty(URL));
-        dataSource.setUsername(environment.getProperty(USER));
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(login);
         dataSource.setPassword(password);
         return dataSource;
     }
-    
+
     @Bean
     @Scope("singleton")
     public JdbcTemplate getJdbcTemplate() {
