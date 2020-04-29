f600c4ead1e156d686aa77252462a0db6f2c71c5
Kirill Rumakin <kirillrumakin@gmail.com>
2020-04-29 21:28:09 +0300

    remove doubled config lines from spring-config-files

--- src/main/java/com/rumakin/universityschedule/config/DatabaseConfig.java
+++ src/main/java/com/rumakin/universityschedule/config/DatabaseConfig.java
@@ -8,51 +8,28 @@ import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.*;
 import org.springframework.jndi.JndiTemplate;
 
 @Configuration
-@ComponentScan("com.rumakin.universityschedule")
-@PropertySource("classpath:persistence-jndi.properties")
 public class DatabaseConfig {
-
-//    @Value("${db.driver}")
-//    private String driver;
-//    @Value("${db.host}")
-//    private String url;
-//    @Value("${db.login}")
-//    private String login;
-//    @Value("${db.password}")
-//    private String password;
 
     private Logger logger = LoggerFactory.getLogger(DatabaseConfig.class);
     
     @Autowired
-    private Environment env;
+    private Environment environment;
 
-//    @Bean
-//    @Scope("singleton")
-//    public DriverManagerDataSource getDataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        logger.info("getDataSource() connect to database: driver {}, url {}, username {}, password {}", driver, url,
-//                login, password);
-//        dataSource.setDriverClassName(driver);
-//        dataSource.setUrl(url);
-//        dataSource.setUsername(login);
-//        dataSource.setPassword(password);
-//        return dataSource;
-//    }
     
     @Bean
     @Scope("singleton")
     public DataSource dataSource() throws NamingException {
-        return (DataSource) new JndiTemplate().lookup(env.getProperty("jdbc.url"));
+        String propertiesPath = environment.getProperty("jdbc.url");
+        logger.info("dataSource() with {}", propertiesPath);
+        return (DataSource) new JndiTemplate().lookup(propertiesPath);
     }
 
     @Bean
     @Scope("singleton")
     public JdbcTemplate getJdbcTemplate() throws NamingException {
-        //return new JdbcTemplate(getDataSource());
         return new JdbcTemplate(dataSource());
     }
 
