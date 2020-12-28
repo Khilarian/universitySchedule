ec078c0fd6c983a186f3322fac10c6fbecc575c9
Kirill Rumakin <donnorvator@gmail.com>
2020-12-28 22:50:54 +0300

    SpringSecurity implemented with inMemoryUser and Role model; TODO: next steps in Spring Security

--- src/main/java/com/rumakin/universityschedule/config/SecurityConfig.java
+++ src/main/java/com/rumakin/universityschedule/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.rumakin.universityschedule.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,20 +12,37 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import com.rumakin.universityschedule.model.enums.Role;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/*").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+        .antMatchers(HttpMethod.GET, "/*/delete/").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.POST, "/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.PUT, "/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE, "/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+                .antMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.PUT, "/api/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE, "/api/**").hasRole(Role.ADMIN.name())
+                .anyRequest().authenticated().and()
+                .httpBasic();
     }
 
     @Bean
     @Override
     protected UserDetailsService userDetailsService() {
         return new InMemoryUserDetailsManager(
-                User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build());
+                User.builder().username("admin").password(passwordEncoder().encode("admin")).roles(Role.ADMIN.name())
+                        .build(),
+                User.builder().username("user").password(passwordEncoder().encode("user")).roles(Role.USER.name())
+                        .build());
     }
 
     @Bean
---
+++ src/main/java/com/rumakin/universityschedule/model/enums/Role.java
@@ -0,0 +1,6 @@
+package com.rumakin.universityschedule.model.enums;
+
+public enum Role {
+
+    USER, ADMIN;
+}
